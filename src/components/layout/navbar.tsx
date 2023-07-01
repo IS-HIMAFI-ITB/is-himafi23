a9c7362ec13ba7f5dc3675fa136f8f805c920cff
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-01 12:12:01 +0800

    Add Navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,122 @@
+import Image, { ImageProps } from "next/image";
+import Link, { LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import logo from "@/../public/logo.svg";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+    NavigationMenuViewport
+} from "@/components/ui/navigation-menu";
+import { useNavbar } from "@/hooks/useNavbar";
+import { cn } from "@/lib/utils";
+import { NavigationMenuLinkProps } from "@radix-ui/react-navigation-menu";
+
+import ThemeSwitch from "../theme-switch";
+import { Button, ButtonProps } from "../ui/button";
+import UserAction from "../user-action";
+
+interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> { }
+
+const NavbarContainer = ({ className, children, ...props }: NavbarProps) => {
+    const { scrolled } = useNavbar()
+    return <div className={cn("sticky backdrop-blur bg-background/30 top-0 w-full transition ease-in", scrolled && 'shadow-lg border-b', className)} {...props}>{children}</div>
+}
+
+const NavbarContent = ({ className, children, ...props }: NavbarProps) => {
+    return <div className={cn("container py-4 flex flex-row items-center justify-evenly", className)} {...props}>{children}</div>
+}
+
+interface NavbarBrandProps extends ImageProps { }
+const NavbarBrand = ({ className, children = null, alt, ...props }: NavbarBrandProps) => {
+    return (
+        <>
+            <Image {...props} alt={alt} className={cn("hover:cursor-pointer", className)} />
+            {children}
+        </>
+    )
+}
+
+const NavbarItems = ({ className, children, ...props }: NavbarProps) => {
+    return <div className={cn("flex flex-row gap-2 items-center mx-auto", className)} {...props}>{children}</div>
+}
+
+type NavbarLinkProps = LinkProps & ButtonProps & { active?: boolean }
+const NavbarLink = ({ className, children, href, ...props }: NavbarLinkProps) => {
+    const active = (usePathname() === href)
+    return (
+        <Button {...props}
+            variant={"link"}
+            className={
+                cn("hover:cursor-pointer",
+                    active && "underline",
+                    className)
+            }
+            asChild
+        >
+            <Link href={href}>{children}</Link>
+        </Button>
+    )
+}
+
+interface NavbarDropdownProps { children: React.ReactNode, trigger: React.ReactNode }
+const NavbarDropdown = ({ trigger, children }: NavbarDropdownProps) => {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>
+                        {trigger}
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        {children}
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuIndicator />
+            </NavigationMenuList>
+            <NavigationMenuViewport />
+        </NavigationMenu>
+    )
+}
+
+interface NavbarDropdownLinkProps extends NavigationMenuLinkProps { }
+const NavbarDropdownLink = ({ className, children, ...props }: NavbarDropdownLinkProps) => {
+    return (
+        <NavigationMenuLink {...props} className={cn(navigationMenuTriggerStyle(), "w-full whitespace-nowrap justify-start")}>{children}</NavigationMenuLink>
+    )
+}
+
+const NavbarSideMenu = ({ className, children, ...props }: NavbarProps) => {
+    return <div className={cn("flex flex-row gap-4 items-center", className)} {...props}>{children}</div>
+}
+
+export default function Navbar() {
+    return (
+        <NavbarContainer>
+            <NavbarContent>
+                <NavbarBrand src={logo} alt="logo" width={32} height={32} />
+                <NavbarItems>
+                    <NavbarLink href="/">Home</NavbarLink>
+                    <NavbarLink href="/materi">Materi</NavbarLink>
+                    <NavbarDropdown trigger="Tentang Kami">
+                        <NavbarDropdownLink href="/#latar-belakang">Latar Belakang</NavbarDropdownLink>
+                        <NavbarDropdownLink href="/#visi-misi">Visi dan Misi</NavbarDropdownLink>
+                        <NavbarDropdownLink href="/#organogram">Organogram</NavbarDropdownLink>
+                    </NavbarDropdown>
+                    <NavbarLink href="/leaderboard">Leaderboard</NavbarLink>
+                </NavbarItems>
+                <NavbarSideMenu>
+                    <ThemeSwitch />
+                    <UserAction loginText="Sign In" />
+                </NavbarSideMenu>
+            </NavbarContent>
+        </NavbarContainer>
+    )
+}
