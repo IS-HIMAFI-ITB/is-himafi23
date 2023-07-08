ab92edfe716cf6374377c3f3f23039814d281c50
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-09 02:32:39 +0800

    remove unneeded imports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,4 @@
 import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import Auth0Provider from "next-auth/providers/auth0";
-import GoogleProvider from "next-auth/providers/google";
-
-import { prisma } from "@/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { authOptions } from "../auth-options";
 
