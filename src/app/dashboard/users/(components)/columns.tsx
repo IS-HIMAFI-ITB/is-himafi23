e32266e27ceac4c2d06bc415c7ac54eec03c1cdb
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-08 18:51:42 +0800

    add datatable

---
+++ src/app/dashboard/users/(components)/columns.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import moment from "moment";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Role, User } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<User>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "nim",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          NIM
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const profile = row.original;
+      return (
+        <div className="flex flex-row w-max items-center gap-2">
+          <Avatar>
+            <AvatarImage src={profile.image ?? undefined} alt={profile.name} />
+            <AvatarFallback>{profile.name.charAt(0)}</AvatarFallback>
+          </Avatar>
+          <span>{profile.name}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "role",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Role
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const role: Role = row.getValue("role");
+      const formatted =
+        role.charAt(0).toUpperCase() + role.slice(1).toLowerCase();
+
+      if (formatted === "Admin")
+        return (
+          <Badge className="bg-yellow-500 hover:bg-yellow-400">
+            {formatted}
+          </Badge>
+        );
+      if (formatted === "Mentor")
+        return (
+          <Badge className="bg-blue-600 hover:bg-blue-500">{formatted}</Badge>
+        );
+      if (formatted === "Student")
+        return <Badge variant={"secondary"}>{formatted}</Badge>;
+
+      return <Badge variant={"outline"}>{formatted}</Badge>;
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ row }) => {
+      const formatted = moment(row.getValue("createdAt")).format("LLLL");
+      return formatted;
+    },
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Updated At",
+    cell: ({ row }) => {
+      const formatted = moment(row.getValue("createdAt")).format("LLLL");
+      return formatted;
+    },
+  },
+  {
+    id: "actions",
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const user = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="w-8 h-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => console.log(user.nim)}>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View user</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
