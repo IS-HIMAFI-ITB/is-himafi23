65556006592d028907308b05c9c615f14fddf146
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-10 12:33:30 +0800

    pisah uploadthing utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,11 @@
-"use client";
-
 import { ClassValue, clsx } from "clsx";
 import { cache } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { QueryClient } from "@tanstack/react-query";
-import { generateReactHelpers } from "@uploadthing/react/hooks";
-
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const getQueryClient = cache(() => new QueryClient());
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
