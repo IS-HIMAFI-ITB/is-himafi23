68f56aca95ff382594a3bfb098f6e9363fa40099
jiroo911 <dan80308@gmail.com>
2023-07-10 11:15:36 +0700

    not found fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-contain bg-gradient-to-r from-indigo-500">
-      <div className="flex flex-col items-center justify-center ">
+    <section className="h-screen bg-contain bg-gradient-to-r from-indigo-500 flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <div className="flex justify-center items-center">
-          <Logo className="mb-12 mt-20" width={100} height={100} />
+          <Logo className="mb-12" width={100} height={100} />
         </div>
         <H1>Error 404</H1>
         <p className="mt-2">The page you were looking does not exist</p>
@@ -16,6 +16,6 @@ export default function Home() {
           <Link href={"/"}>Return to home</Link>
         </Button>
       </div>
-    </div>
+    </section>
   );
 }
