6442673940a9648df3bb5d8b4d849c359201913b
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-11 20:32:06 +0800

    add zod form validation on login

--- src/app/login/(components)/login-card.tsx
+++ src/app/login/(components)/login-card.tsx
@@ -3,8 +3,10 @@
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 import Container from "@/components/layout/container";
 import Logo from "@/components/logo";
@@ -20,8 +22,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/toast/useToast";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import GoogleImage from "./google-icon";
 
@@ -29,13 +40,31 @@ interface LoginCardProps {
   className?: string;
 }
 
+const loginSchema = z.object({
+  nim: z.coerce
+    .number()
+    .gte(10221000, "NIM must be in range of 10221000-10222120")
+    .lte(10222120, "NIM must be in range of 10221000-10222120"),
+  password: z.string().nonempty("Password must not be empty"),
+});
+
 const LoginCard: React.FC<LoginCardProps> = ({ className }) => {
   const router = useRouter();
   const { status } = useSession();
-  const [form, setForm] = useState({
-    nim: Number(),
-    password: "",
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
   });
+
+  const { toast } = useToast();
+
+  function onSubmit(data: z.infer<typeof loginSchema>) {
+    toast({
+      title: "Logging in...",
+      description:
+        "Tahukah kamu bahwa Prof. Ir. R.O Kosasih adalah rektor pertama ITB? Beliau merupakan guru besar Teknik Elektro.",
+    });
+    signIn("credentials", { callbackUrl: "/", ...data });
+  }
 
   if (status === "loading") return <Loading />;
 
@@ -60,59 +89,56 @@ const LoginCard: React.FC<LoginCardProps> = ({ className }) => {
         <CardTitle>Log In</CardTitle>
         <CardDescription>Enter your login details</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Username</Label>
-              <Input
-                id="name"
-                placeholder="Enter your username"
-                onChange={(e) =>
-                  setForm({ ...form, nim: Number(e.target.value) })
-                }
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">
-                <div className="flex flex-row justify-between">
-                  <span>Password</span>
-                  <Button
-                    variant={"link"}
-                    className="text-xs -mt-3 -mb-5 px-0 opacity-70 justify-self-end transition"
-                    asChild
-                  >
-                    <Link href={""}>Forgot Password</Link>
-                  </Button>
-                </div>
-              </Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="Enter your password"
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
-              />
-            </div>
-          </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="nim"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>NIM</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Masukkan NIM TPB kamu"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              variant={"link"}
+              className="text-xs -mb-5 px-0 opacity-70 justify-self-end transition"
+              asChild
+            >
+              <Link href={""}>Forgot Password</Link>
+            </Button>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">Log in</Button>
+            <Button variant={"secondary"} className="ml-4">
+              <GoogleImage height={20} width={20} />
+              <span>Register with Google</span>
+            </Button>
+          </CardFooter>
         </form>
-      </CardContent>
-      <CardFooter>
-        <Button
-          onClick={() =>
-            signIn("credentials", {
-              nim: form.nim,
-              password: form.password,
-              callbackUrl: "/",
-            })
-          }
-        >
-          Log in
-        </Button>
-        <Button variant={"secondary"} className="ml-4">
-          <GoogleImage height={20} width={20} />
-          <span>Register with Google</span>
-        </Button>
-      </CardFooter>
+      </Form>
     </Card>
   );
 };
