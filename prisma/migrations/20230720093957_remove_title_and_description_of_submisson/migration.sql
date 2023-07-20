0e20a31418b5de80851a34f9c5e441ca8c72f454
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-07-20 20:07:15 +0800

    delete title and desc column from submission model

---
+++ prisma/migrations/20230720093957_remove_title_and_description_of_submisson/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Submission` table. All the data in the column will be lost.
+  - You are about to drop the column `title` on the `Submission` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Submission` DROP COLUMN `description`,
+    DROP COLUMN `title`;
