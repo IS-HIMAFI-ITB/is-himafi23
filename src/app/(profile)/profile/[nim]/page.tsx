d03ed3cd04e086353953f54d4d35cabfb29d7074
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-08-06 15:12:42 +0800

    general animate-section component

--- src/app/(profile)/profile/[nim]/page.tsx
+++ src/app/(profile)/profile/[nim]/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 
 import { authOptions } from "@/app/api/auth/auth-options";
+import AnimateSection from "@/components/animate-section";
 import Container from "@/components/layout/container";
 import Unauthenticated from "@/components/template/unauthenticated";
 import { prisma } from "@/prisma";
@@ -10,7 +11,6 @@ import AdditionalDetails from "./(components)/(additional details)/additional-de
 import UserPosts from "./(components)/(posts)/user-posts";
 import ProfileCard from "./(components)/(profile card)/profile-card";
 import SocialMedia from "./(components)/(social media)/social-media";
-import AnimateSection from "./(components)/animate-section";
 
 // Nanti buat params id ini bakal pake NIM, jadi klo mau liat profile orang lain tinggal masukin NIMnya aja, sama kalo pake UUID ribet awikwok
 export default async function ProfilePage({
@@ -31,16 +31,41 @@ export default async function ProfilePage({
 
   return (
     <Container className="min-h-[calc(100vh-72.6px-4rem)] h-full flex flex-col gap-3 justify-center w-max">
-      <AnimateSection className="flex flex-col lg:flex-row gap-4 justify-between mt-40">
+      <AnimateSection
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.8,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay: 0,
+        }}
+        className="flex flex-col lg:flex-row gap-4 justify-between mt-40"
+      >
         <ProfileCard user={user} />
         <SocialMedia user={user} />
       </AnimateSection>
 
-      <AnimateSection>
+      <AnimateSection
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.8,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay: 0,
+        }}
+      >
         <AdditionalDetails user={user} />
       </AnimateSection>
 
-      <AnimateSection>
+      <AnimateSection
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.8,
+          ease: [0, 0.71, 0.2, 1.01],
+          delay: 0,
+        }}
+      >
         <UserPosts user={user} />
       </AnimateSection>
     </Container>
---
+++ src/components/animate-section.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion, MotionProps } from "framer-motion";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+type AnimateSectionProps = MotionProps & {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export default function AnimateSection(props: AnimateSectionProps) {
+  return (
+    <motion.section {...props} className={cn(props.className)}>
+      {props.children}
+    </motion.section>
+  );
+}
