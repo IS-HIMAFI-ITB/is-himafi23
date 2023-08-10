aa85883597e36d5da4e5ea2bd067ff10b97bdbce
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-08-11 01:23:22 +0800

    next response error on catch

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/prisma";
 export async function GET(req: NextRequest) {
   const url = req.url;
   const result = await prisma.user.findMany({}).catch((err: Error) => {
-    return NextResponse.json(err, { status: 500 });
+    return NextResponse.error();
   });
 
   return NextResponse.json(result, { status: 200 });
